e5c8df4a21cd87676cdc1a1d2e4f43ff67262c33
toyhammered <daniel@rassiner.com>
2016-03-08 15:27:11 -0800

    raised and authenticated user correctly

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,12 +1,15 @@
 require "kele/version"
 require "httparty"
+require "kele/errors"
 
 class Kele
   include HTTParty
 
   def initialize(email, password)
-    # auth_token = response["auth_token"]
-    # url = "https://www.bloc.io/api/v1/sessions"
-    # response = self.class.post("#{url}?email=#{email}&password=#{password}")
+    url = "https://www.bloc.io/api/v1/sessions"
+    response = self.class.post("#{url}?email=#{email}&password=#{password}")
+
+    raise InvalidStudentCodeError.new() if response.code == 401
+    @auth_token = response["auth_token"]
   end
 end
--- spec/kele_spec.rb
+++ spec/kele_spec.rb
@@ -3,20 +3,16 @@ require 'spec_helper'
 describe Kele, type: :request do
   context '.kele' do
     describe '#initialize' do
-      it 'returns error with invalid email or password' do
-        email = ENV['BLOC_EMAIL']
-        password = "fakefake"
-        response = HTTParty.post("https://www.bloc.io/api/v1/sessions?email=#{email}&password=#{password}")
-        puts response.header.inspect
-        expect(response.header).to raise_error(Net::HTTPUnauthorized)
+
+      it 'raises error' do
+        expect { Kele.new(ENV['BLOC_EMAIL'], "fakefake") }.to raise_error(InvalidStudentCodeError)
       end
 
-      it 'returns auth token' do
-        email = ENV['BLOC_EMAIL']
-        password = ENV['BLOC_PASSWORD']
-        response = HTTParty.post("https://www.bloc.io/api/v1/sessions?email=#{email}&password=#{password}")
-        expect(response.header).to have_http_status(:success)
+      it 'authenticates user' do
+        response = Kele.new(ENV['BLOC_EMAIL'], ENV['BLOC_PASSWORD'])
+        expect(response.instance_variable_get(:@auth_token)).to_not be_nil
       end
+
     end
   end
 
