050e32205a995ef1e92a21ceae51871928690731
toyhammered <daniel@rassiner.com>
2016-03-08 12:35:03 -0800

    writing tests for authorization

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,7 +1,12 @@
 require "kele/version"
+require "httparty"
 
 class Kele
-  def initialize(email, password)
+  include HTTParty
 
+  def initialize(email, password)
+    # auth_token = response["auth_token"]
+    # url = "https://www.bloc.io/api/v1/sessions"
+    # response = self.class.post("#{url}?email=#{email}&password=#{password}")
   end
 end
--- spec/kele_spec.rb
+++ spec/kele_spec.rb
@@ -1,12 +1,23 @@
 require 'spec_helper'
 
-describe Kele do
-  it 'has a version number' do
-    expect(Kele::VERSION).not_to be nil
+describe Kele, type: :request do
+  context '.kele' do
+    describe '#initialize' do
+      it 'returns error with invalid email or password' do
+        email = ENV['BLOC_EMAIL']
+        password = "fakefake"
+        response = HTTParty.post("https://www.bloc.io/api/v1/sessions?email=#{email}&password=#{password}")
+        puts response.header.inspect
+        expect(response.header).to raise_error(Net::HTTPUnauthorized)
+      end
+
+      it 'returns auth token' do
+        email = ENV['BLOC_EMAIL']
+        password = ENV['BLOC_PASSWORD']
+        response = HTTParty.post("https://www.bloc.io/api/v1/sessions?email=#{email}&password=#{password}")
+        expect(response.header).to have_http_status(:success)
+      end
+    end
   end
 
-  it 'does something useful' do
-    user = Kele.new("test", "test1")
-    expect(user).not_to be nil
-  end
 end
