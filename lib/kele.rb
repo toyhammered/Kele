3d86c9f76cebf2417c23e4b2e141b10c5878dfef
toyhammered <daniel@rassiner.com>
2016-03-19 21:22:33 -0700

    create_message allows you to create new message, can't respond to existing message yet

--- lib/kele.rb
+++ lib/kele.rb
@@ -40,6 +40,20 @@ class Kele
     @checkpoint = JSON.parse(response.body)
   end
 
+  def get_messages(page)
+    response = self.class.get(api_url("message_threads?page=#{page}"), headers: { "authorization": @auth_token })
+    # count = (response['count']/10.to_f).ceil
+    # not sure how to make all pages load together
+
+    @get_messages = JSON.parse(response.body)
+    pp @get_messages
+  end
+
+  def create_message(recipient_id, subject, message)
+    response = self.class.post(api_url("messages"), body: { "user_id": id, "recipient_id": recipient_id, "subject": subject, "stripped-text": message }, headers: { "authorization": @auth_token })
+    puts response
+  end
+
   private
 
   def api_url(endpoint)
