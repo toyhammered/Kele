0412fd51f71697eb86ff0adef5e46b966dcac300
toyhammered <daniel@rassiner.com>
2016-04-04 16:29:17 -0700

    added current_enrollment for create submission

--- lib/kele.rb
+++ lib/kele.rb
@@ -35,7 +35,7 @@ class Kele
   end
 
   def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment)
-    response = self.class.post(api_url("checkpoint_submissions"), :body => { "checkpoint_id" => checkpoint_id, "assignment_branch" => assignment_branch, "assignment_commit_link" => assignment_commit_link, "comment" => comment }.merge(user_auth))
+    response = self.class.post(api_url("checkpoint_submissions"), :body => { "enrollment_id" => current_enrollment["id"], "checkpoint_id" => checkpoint_id, "assignment_branch" => assignment_branch, "assignment_commit_link" => assignment_commit_link, "comment" => comment }.merge(user_auth))
   end
 
   def api_url(endpoint)
